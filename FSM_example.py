c6d1cb1ebb64854e8190144e263fdc934834f6ee
Dawid Cyganek <28643223+hhimko@users.noreply.github.com>
2022-05-31 10:53:50 +0200

    minor SFM fixes

--- FSM_example.py
+++ FSM_example.py
@@ -9,8 +9,8 @@ from ev3dev2.sensor.lego import UltrasonicSensor, InfraredSensor # type: ignore
 from ev3dev2.button import Button # type: ignore
 
 
-MAX_PROXIMITY_TO_OBSTACLE  = 15.0  
-MIN_DISTANCE_FROM_OBSTACLE = 25.0
+MAX_PROXIMITY_TO_OBSTACLE  = 25.0  
+MIN_DISTANCE_FROM_OBSTACLE = 40.0
 
 
 # instanciate an ev3dev2.Button object to let us control the robot
@@ -18,7 +18,7 @@ button = Button()
 
 # define a world of point coordinates for the robot to move through
 waypoints = (
-    Point(200, 0), Point(125, 100), Point(125, 200)
+    Point(0, -350),
 )
 
 # define robot sensors which will be used to measure distance from obstacles
@@ -30,7 +30,7 @@ sensors = (
 weights = (1, 2, 1)
 
 # create a robot, that we'll be controlling
-robot = EducatorRobot(speed=60)
+robot = EducatorRobot(speed=50)
 
 STATE_GTG = GTGController(robot)
 STATE_AO  = AOController(robot, bias=50, P=100)
@@ -38,10 +38,12 @@ current_state = STATE_GTG
 
 
 curr_waypoint_idx = 0
-while curr_waypoint_idx < len(waypoints):
+while curr_waypoint_idx < len(waypoints) and not button.enter:
+    print(current_state)
 
-    with current_state as controller:
-        if isinstance(controller, GTGController):
+    state = current_state
+    with state as controller:
+        if current_state == STATE_GTG:
             curr_waypoint = waypoints[curr_waypoint_idx]
             print("[INFO] Moving to {}..".format(curr_waypoint))
             
@@ -57,8 +59,8 @@ while curr_waypoint_idx < len(waypoints):
                     print("[INFO] Waypoint successfully reached!\n")
                     curr_waypoint_idx += 1
                     break
-            
-        elif isinstance(controller, AOController):
+
+        elif current_state == STATE_AO:
             while not button.enter:
                 controller.update(sensors, weights)
                 
@@ -66,15 +68,4 @@ while curr_waypoint_idx < len(waypoints):
                     print("[INFO] Going To Goal")
                     current_state = STATE_GTG
                     break
-
-
-
-# with GTGController(robot) as controller:
-#     # call GTGController.gotogoal on each waypoint until it's reached by the robot
-#     for waypoint in waypoints:
-#         print("[INFO] Moving to {}..".format(waypoint))
-        
-#         while not controller.reached(waypoint):
-#             controller.gotogoal(waypoint)
-            
-#         print("[INFO] Waypoint successfully reached!\n")
+             
